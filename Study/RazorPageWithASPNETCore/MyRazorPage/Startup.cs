f100ca7ac4419236c3aab979d55371b946cfb064
Howard Chiang <v-hochia@microsoft.com>
2018-10-18 10:53:10 +0800

    看RazorPage相關memo

--- Study/RazorPageWithASPNETCore/MyRazorPage/Startup.cs
+++ Study/RazorPageWithASPNETCore/MyRazorPage/Startup.cs
@@ -49,7 +49,49 @@ namespace MyRazorPage
             }
 
             app.UseHttpsRedirection();
+
+            #region app.UseStaticFiles()相關說明
+            //決定預設wwwroot下的static file path為root之下：ex:http://localhost/images/banner1.svg 
+            //若要客製在別的目錄之下：http://bit.ly/2PBacxu
+            //
             app.UseStaticFiles();
+
+            //將實體目錄MyStaticFiles註冊成/StaticFiles位置
+#if false
+             app.UseStaticFiles(new StaticFileOptions
+             {
+                 FileProvider = new PhysicalFileProvider(
+            Path.Combine(Directory.GetCurrentDirectory(), "MyStaticFiles")),
+                 RequestPath = "/StaticFiles"
+             });
+#endif
+
+            //也可以設定Static File Response的Header資訊，ex:決定cache多長之類 http://bit.ly/2PEqHJ5
+#if false
+            app.UseStaticFiles(new StaticFileOptions
+            {
+                OnPrepareResponse = ctx =>
+                {
+                    // Requires the following import:
+                    // using Microsoft.AspNetCore.Http;
+                    ctx.Context.Response.Headers.Append("Cache-Control", $"public, max-age={cachePeriod}");
+                }
+            });
+#endif
+            //預設wwwroot是無認證機制的，若asset要防護，則放至wwwroot之外，並用Action去控管  http://bit.ly/2PE7lUu
+#if false
+[Authorize]
+            public IActionResult BannerImage()
+        {
+            var file = Path.Combine(Directory.GetCurrentDirectory(),
+                                    "MyStaticFiles", "images", "banner1.svg");
+
+            return PhysicalFile(file, "image/svg+xml");
+        }
+#endif
+            #endregion
+
+
             app.UseCookiePolicy();
 
             app.UseMvc();
